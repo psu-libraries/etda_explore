0179d08e1164ca85b536317c3ded35745ed6c0c6
whereismyjetpack <djb44@psu.edu>
2022-06-20 15:32:38 -0400

    remove commented code

--- app/helpers/blacklight_display_helper.rb
+++ app/helpers/blacklight_display_helper.rb
@@ -76,9 +76,5 @@ module BlacklightDisplayHelper
     def facet_link(value, field)
       link_to(value, "/?f[#{field}][]=#{CGI.escape value}")
     end
-
-    # def current_user
-      # current_or_guest_user
-      # ApplicationController::HelperMethods.current_user
-    # end
+  
 end
