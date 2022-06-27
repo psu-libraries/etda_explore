f7e91b4c6137b03ca6e2f5c2fe2906a8dd2a6e4d
Alex Kiessling <ajkiessl@gmail.com>
2022-06-27 11:56:54 -0400

    Legacy redirect routes, legacy redirect controller, and some logic in the legacy redirect controller to render 404 when no record is found.  Tests for this.

---
+++ app/controllers/legacy_redirect_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class LegacyRedirectController < ApplicationController
+  def redirect_original_urls
+    bl_object = Blacklight::Solr::Repository.new(CatalogController.blacklight_config)
+    bl_response = bl_object.search(q: "db_legacy_old_id:#{params[:id]}")
+    doc = bl_response.documents.first || nil
+    new_id = doc.present? ? doc.id : nil
+    if new_id.present?
+      redirect_to "/catalog/#{new_id}", status: 301
+    else
+      render template: '/error/404', formats: [:html, :json], status: 404
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,14 @@ Rails.application.routes.draw do
   get '/about', to: 'application#about'
   get '/files/final_submissions/:id', to: 'files#solr_download_final_submission', as: :final_submission_file
 
+  # Legacy Redirects
+  get '/theses/approved/:access_level/:id_prefix-:id/(:all)',
+      to: 'legacy_redirect#redirect_original_urls',
+      constraints:  { id_prefix: /E(:?HT|TD)/, access_level: /WorldWideIndex|PSUonlyIndex|WithheldIndex/ }
+  get '/paper/:id/(:all)', to: redirect('/catalog/%{id}'), status: 301
+  get '/search', to: redirect('/catalog'), status: 301
+  get '/browse', to: redirect('/catalog'), status: 301
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
