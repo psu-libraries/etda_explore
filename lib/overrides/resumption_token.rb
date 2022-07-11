7ed94700e7be824471feb361aae64d7837e3a931
whereismyjetpack <djb44@psu.edu>
2022-07-11 15:20:24 -0400

    rubocop

--- lib/overrides/resumption_token.rb
+++ lib/overrides/resumption_token.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 module BlacklightOaiProvider
   class ResumptionToken < ::OAI::Provider::ResumptionToken
     # Override encode conditions to allow for parsing date / datetime objects
-    # https://github.com/code4lib/ruby-oai/pull/97 
+    # https://github.com/code4lib/ruby-oai/pull/97
     def encode_conditions
       encoded_token = @prefix.to_s.dup
       encoded_token << ".s(#{set})" if set
-      if self.from
-        if self.from.respond_to?(:utc)
-          encoded_token << ".f(#{self.from.utc.xmlschema})"
-        else
-          encoded_token << ".f(#{self.from.xmlschema})"
-        end
+      if from
+        encoded_token << if from.respond_to?(:utc)
+                           ".f(#{from.utc.xmlschema})"
+                         else
+                           ".f(#{from.xmlschema})"
+                         end
       end
       if self.until
-        if self.until.respond_to?(:utc)
-          encoded_token << ".u(#{self.until.utc.xmlschema})" 
-        else
-          encoded_token << ".u(#{self.until.xmlschema})" 
-        end
+        encoded_token << if self.until.respond_to?(:utc)
+                           ".u(#{self.until.utc.xmlschema})"
+                         else
+                           ".u(#{self.until.xmlschema})"
+                         end
       end
       encoded_token << ".t(#{total})" if total
       encoded_token << ":#{last}"
