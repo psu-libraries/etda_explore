8b6c24637dfa8b54acccf3f57e4a6de3c3a95806
Justin Patterson <jrp22@psu.edu>
2025-03-31 12:39:26 -0400

    We do not want to protect the /health from the bot checks.

--- config/initializers/bot_challenge_page.rb
+++ config/initializers/bot_challenge_page.rb
@@ -23,7 +23,7 @@ Rails.application.config.to_prepare do
 
   # How long will a challenge success exempt a session from further challenges?
   BotChallengePage::BotChallengePageController.bot_challenge_config.session_passed_good_for = 1.hour
-  BotChallengePage::BotChallengePageController.allow_exempt = ->(controller) {
+  BotChallengePage::BotChallengePageController.bot_challenge_config.allow_exempt = ->(controller) {
     controller.request.path == "/health"
   }
 
