f343181cd18861091d7507835c03f2fef1916e16
Alex Kiessling <ajkiessl@gmail.com>
2025-12-22 16:11:04 -0500

    Adds in API Key to request.  Adds envrc examples

--- app/services/auto_remediate_webhook_service.rb
+++ app/services/auto_remediate_webhook_service.rb
@@ -7,12 +7,20 @@ class AutoRemediateWebhookService
 
   def call
     payload = { final_submission_file_id: @final_submission_file_id }
-    response = Faraday.post("#{base_url}#{auto_remediate_webhook_path}",
-                            payload.to_json,
-                            { 'Content-Type' => 'application/json' })
+    headers = {
+      'Content-Type' => 'application/json',
+      'X-API-Key' => auto_remediate_webhook_api_key
+    }
+
+    response = Faraday.post(
+      "#{base_url}#{auto_remediate_webhook_path}",
+      payload.to_json,
+      headers
+    )
 
     unless response.status == 200
-      raise "AutoRemediateWebhookService: HTTP #{response.status} response body=#{response.body.to_s[0, 500]}"
+      raise "AutoRemediateWebhookService: HTTP #{response.status} " \
+            "response body=#{response.body.to_s[0, 500]}"
     end
 
     response
@@ -22,8 +30,12 @@ class AutoRemediateWebhookService
 
     attr_reader :final_submission_file_id
 
+    def auto_remediate_webhook_api_key
+      ENV.fetch('AUTO_REMEDIATE_WEBHOOK_API_KEY')
+    end
+
     def auto_remediate_webhook_path
-      ENV.fetch('AUTO_REMEDIATE_WEBHOOK_PATH', nil)
+      ENV.fetch('AUTO_REMEDIATE_WEBHOOK_PATH')
     end
 
     def base_url
