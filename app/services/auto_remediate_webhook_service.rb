19e07655e3a591eeebf065e682e69a57b3dca880
Alex Kiessling <ajkiessl@gmail.com>
2026-01-29 15:28:08 -0500

    Adds faraday-retry and implements retry configuration

--- app/services/auto_remediate_webhook_service.rb
+++ app/services/auto_remediate_webhook_service.rb
@@ -6,25 +6,37 @@ class AutoRemediateWebhookService
   end
 
   def notify
-    payload = { final_submission_file_id: final_submission_file_id }
-    headers = {
-      'Content-Type' => 'application/json',
-      'X-API-Key' => auto_remediate_webhook_token
-    }
-
-    conn = Faraday.new(
-      url: "#{base_url}#{auto_remediate_webhook_path}",
-      headers: headers
-    )
-
-    conn.post do |req|
-      req.body = payload.to_json
+    connection.post do |req|
+      req.body = { final_submission_file_id: final_submission_file_id }.to_json
     end
   end
 
   private
 
     attr_reader :final_submission_file_id
+
+    def connection
+      # Retries up to 5 times, doubling the wait each time (exponential backoff).
+      Faraday.new(url: url, headers: headers) do |f|
+        f.request :retry,
+                  max: 5,
+                  interval: 0.7,
+                  backoff_factor: 2,
+                  exceptions: Faraday::Retry::Middleware::DEFAULT_EXCEPTIONS
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def url
+      "#{base_url}#{auto_remediate_webhook_path}"
+    end
+
+    def headers
+      {
+        'Content-Type' => 'application/json',
+        'X-API-Key' => auto_remediate_webhook_token
+      }
+    end
 
     def auto_remediate_webhook_token
       ENV.fetch('AUTO_REMEDIATE_WEBHOOK_TOKEN')
