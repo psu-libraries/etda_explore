5ccc6660a146bb78539f0819989dfdabe5cc9bfa
Smulligan <smm5878@psu.edu>
2026-07-14 15:22:06 -0400

    niftany and styling

--- app/components/blacklight/search/sidebar_component.rb
+++ app/components/blacklight/search/sidebar_component.rb
@@ -4,16 +4,17 @@ module Blacklight
   module Search
     class SidebarComponent < Blacklight::Component
       def initialize(blacklight_config:, response:, view_config:)
+        super
         @blacklight_config = blacklight_config
         @response = response
         @group_component_class = view_config.facet_group_component
       end
 
-def visible_facet_fields(groupname)
-  facet_fields_in_group(groupname).select do |field|
-    response.facet_fields[field.field].present?
-  end
-end
+      def visible_facet_fields(groupname)
+        facet_fields_in_group(groupname).select do |field|
+          response.facet_fields[field.field].present?
+        end
+      end
 
       attr_reader :group_component_class, :response
 
