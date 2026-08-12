03361b417d8b02830412c322818d81f7265606c1
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-08-12 14:34:39 -0600

    refactor

--- app/helpers/blacklight_display_helper.rb
+++ app/helpers/blacklight_display_helper.rb
@@ -79,6 +79,7 @@ module BlacklightDisplayHelper
     end
 
     def final_submission_links(document)
+      defaults = { 'turbo-prefetch': false }
       document.final_submissions.map do |final_submission_id, name|
         query_params = { remediated: 'false', remediate_token: remediate_token(final_submission_id) }
         should_show_modal = document.remediated_final_submissions.blank? && ENV['ENABLE_ACCESSIBILITY_REMEDIATION'] == 'true'
@@ -86,17 +87,14 @@ module BlacklightDisplayHelper
         modal_trigger_options = if should_show_modal
                                   { 'bs-toggle': 'modal',
                                     'bs-target': '#downloadModal',
-                                    'turbo-prefetch': false,
                                     file_path: file_path }
-                                else
-                                  { 'turbo-prefetch': false }
                                 end
-
+        data_options = defaults.merge(modal_trigger_options || {})
         content_tag(:span,
                     link_to(tag.i(class: 'fa fa-download download-link-fa') + "Download #{name}",
                             file_path,
                             onclick: document.confirmation ? "return confirm('#{document.confirmation}')" : nil,
-                            data: modal_trigger_options,
+                            data: data_options,
                             class: 'file-link form-control download-trigger'))
       end
     end
